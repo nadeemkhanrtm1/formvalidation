63020b306901aa694c209e4706002430b9c72d3a
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2020-09-30 21:52:35 +0530

    Updates

---
+++ precache-manifest.eca996d5c69843b72471d2e5aa1a548e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d849748a93dc545786d268a2ec76170f",
+    "url": "/nadeemkhanrtm1/formvalidation.git/index.html"
+  },
+  {
+    "revision": "bf84a81fc5e6bcca63c1",
+    "url": "/nadeemkhanrtm1/formvalidation.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c02f25122bb26d5e6ad3",
+    "url": "/nadeemkhanrtm1/formvalidation.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf84a81fc5e6bcca63c1",
+    "url": "/nadeemkhanrtm1/formvalidation.git/static/js/2.df2006e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nadeemkhanrtm1/formvalidation.git/static/js/2.df2006e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02f25122bb26d5e6ad3",
+    "url": "/nadeemkhanrtm1/formvalidation.git/static/js/main.a1eb51fc.chunk.js"
+  },
+  {
+    "revision": "dce1c532b8a4b197b215",
+    "url": "/nadeemkhanrtm1/formvalidation.git/static/js/runtime-main.a2c11966.js"
+  }
+]);
